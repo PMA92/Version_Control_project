2998a29e17be481562761e2d9866ed5929f72532
PMA92 <paulabruzzo8@gmail.com>
2025-05-22 23:06:59 -0400

    worked on indexing, added hash table feature

--- src/add.c
+++ src/add.c
@@ -3,6 +3,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define TABLE_SIZE 1000
+
+typedef struct index{
+    char *filename;
+    char *hash;
+} Index;
+
+typedef struct hashtable{
+    Index *files[TABLE_SIZE];
+} HashTable;
+
+HashTable *createTable(){
+    HashTable *table = malloc(sizeof(HashTable));
+    if (!table) {
+        perror("Memory allocation failed");
+        exit(1);
+    }
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        table->files[i] = NULL;
+    }
+    return table;
+}
+
+HashTable *searchTable(HashTable *table, char *filename){
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (table->files[i] != NULL && strcmp(table->files[i]->filename, filename) == 0) {
+            return table->files[i];
+        }
+    }
+    return NULL;
+}
+
+HashTable *appendFile(HashTable *table, Index *index){
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (table->files[i] == NULL) {
+            table->files[i] = index;
+            return table;
+        }
+    }
+}
+
+HashTable *removeFile(HashTable *table, char *filename){
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (table->files[i] != NULL && strcmp(table->files[i]->filename, filename) == 0) {
+            free(table->files[i]);
+            table->files[i] = NULL;
+            return table;
+        }
+    }
+}
 
 char *hash(FILE *file, unsigned char *buffer, unsigned char **outContent, long *outContentLen){    
     if (!file) {
@@ -52,6 +102,14 @@ int addFile(char *filename)
 {
     FILE *file = fopen(filename, "rb");
     
+    Index *index = malloc(sizeof(Index));
+    index->filename = filename;
+    index->hash = NULL;
+    if (!index) {
+        perror("Memory allocation failed");
+        return 1;
+    }
+
     if (!file) {
         perror("Error opening file");
         return 1;
@@ -67,6 +125,19 @@ int addFile(char *filename)
 
     char *hash_hex = hash(file, hash_buffer, &outContent, &outContentLen);
     fclose(file);
+
+    index->hash = hash_hex;
+
+    HashTable *table = malloc(sizeof(HashTable));
+    if (!table) {
+        perror("Memory allocation failed");
+        free(index);
+        free(hash_hex);
+        free(outContent);
+        return 1;
+    }
+
+
 
 
     char newBlobName[256];
@@ -87,5 +158,20 @@ int addFile(char *filename)
         free(outContent);
         return 1;
     }
+    
+    HashTable *existingIndex = searchTable(table, filename);
+
+    if (!existingIndex) {
+        fopen(".mockgit/index", "a");
+        //read and add the filename and hash to the index file 
+        table = appendFile(table, index);
+        printf("File added: %s\n", filename);
+    } else {
+        printf("File already exists in the index: %s\n", filename);
+        free(index->hash);
+        free(index);
+    }  
+    
+    
     return 0;
 }
